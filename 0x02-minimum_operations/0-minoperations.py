c0f50a689f55f3073677b6c134c4eda4664fc20e
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-14 00:22:52 +0100

    adding files

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''finding the min operation'''
+
+
+def minOperations(n):
+    '''the algorithm starts here'''
+    div, min_op = 2, 0
+    while n > 1:
+        if n % div == 0:
+            n = n / div
+            min_op += div
+        else:
+            div += 1
+    return min_op
