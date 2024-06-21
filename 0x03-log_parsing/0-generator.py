c6351779621afc662f5044e301022dd63a0a0f85
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-06-21 04:55:16 +0100

    adding

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-'''file to generate for stdin'''
 import random
+import sys
 from time import sleep
-from datetime import datetime
-from sys import stdout
+import datetime
 
-
-stat_code = [200, 301, 400, 401, 403, 404, 405, 500]
-for i in range(1000):
+for i in range(10000):
     sleep(random.random())
-    stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-           random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-           random.randint(1, 255), datetime.now(),
-           random.choice(stat_code), random.randint(1, 1024)
-           ))
-    stdout.flush()
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(
+            1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
+    ))
+    sys.stdout.flush()
