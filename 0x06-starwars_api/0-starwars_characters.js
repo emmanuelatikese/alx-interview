f6439cdf3c77778483e60bc243d61bee80670fe1
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-11 19:38:29 +0100

    adding files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,20 +2,20 @@
 const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/3/';
-request.get(url, (err, body) => {
+request.get(url, (err, data) => {
   if (err) {
-    console.error(`Error: ${err}`);
+    console.error(err);
     return;
   }
-
-  const filmData = JSON.parse(body);
-  const characters = filmData.characters;
-  characters.foreach(x => {
-    request.get(x, (data) => {
-      const charactersName = JSON.parse(data);
-      if (charactersName) {
-        console.log(charactersName.name);
+  const bodyJson = JSON.parse(data.body).characters;
+  bodyJson.forEach(x => {
+    request.get(x, (err, body) => {
+      if (err) {
+        console.error(err);
+        return;
       }
+      const people = JSON.parse(body.body);
+      console.log(people.name);
     });
   });
 });
