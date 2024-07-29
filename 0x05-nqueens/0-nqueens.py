6c9a6c4cd5e1b7fa911ed9b468c5e81398a14a61
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-29 16:07:07 +0100

    addding

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         print("Usage: nqueens N")
         exit(1)
     try:
-        n = int(argv[1])
+        n= int(argv[1])
         if n < 4:
             print("N must be at least 4")
             exit(1)
